b2d54d4ed2ecb40c33d9198b57400061c5d366f8
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-12-31 23:17:37 +0530

    user entity

--- backend/src/main/java/com/example/backend/entity/User.java
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -1,11 +1,13 @@
 package com.example.backend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Entity
@@ -14,5 +16,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+//    private String name;
+//    private String email;
+//    private String password;
+//    private  String phoneNumber;
+//    private UserRole role;
+//
+//    private List<OrderItem>orderItemList;
+//    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+//    private Address address;
+//    private final LocalDateTime createdAt=LocalDateTime.now();
+
 
 }
