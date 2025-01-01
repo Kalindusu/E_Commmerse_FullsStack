0d6c3cd8a48f58a2f53b45b187039a3c81d2e71f
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2025-01-01 23:35:11 +0530

    User entity complete

--- backend/src/main/java/com/example/backend/entity/User.java
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -1,6 +1,7 @@
 package com.example.backend.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,16 +20,29 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @Column(unique = true)
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @NotBlank(message = "Password number is required")
     private String password;
+
+    @Column(name = "phone_number")
+    @NotBlank(message = "Phone number is required")
     private  String phoneNumber;
+
     private UserRole role;
 
-    private List<OrderItem>orderItemList;
-    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<OrderItem> orderItemList;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "user")
     private Address address;
-    private final LocalDateTime createdAt=LocalDateTime.now();
 
-
+    @Column(name = "created_at")
+    private final LocalDateTime createdAt = LocalDateTime.now();
 }
