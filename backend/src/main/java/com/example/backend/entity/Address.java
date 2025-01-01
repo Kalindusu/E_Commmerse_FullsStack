f3729e65557fa3e3225613b43ebcdf0736201454
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2025-01-01 23:40:11 +0530

    Address entity date

--- backend/src/main/java/com/example/backend/entity/Address.java
+++ backend/src/main/java/com/example/backend/entity/Address.java
@@ -1,4 +1,20 @@
 package com.example.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="addresses")
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String street;
+    private String city;
+    private String state;
+    private String zipcode;
+    private String country;
+
 }
