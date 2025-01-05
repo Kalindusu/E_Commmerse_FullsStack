aea1505f3b9a415bf16ef44bc3b75d6fc2dd5b1d
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2025-01-05 18:33:35 +0530

    OrderItem entity update

--- backend/src/main/java/com/example/backend/entity/OrderItem.java
+++ backend/src/main/java/com/example/backend/entity/OrderItem.java
@@ -1,10 +1,13 @@
 package com.example.backend.entity;
 
+import com.example.backend.enums.OrderStatus;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+
+import java.math.BigDecimal;
 
 @Data
 @Entity
@@ -12,5 +15,8 @@ public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private BigDecimal price;
+    private OrderStatus status;
+    private  User user;
 
 }
