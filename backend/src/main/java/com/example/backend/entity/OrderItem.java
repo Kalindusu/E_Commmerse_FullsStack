295f6e7c83ef8feb79611027cf773ee9b6de42e1
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2025-01-05 18:32:22 +0530

    OrderItem entity update

--- backend/src/main/java/com/example/backend/entity/OrderItem.java
+++ backend/src/main/java/com/example/backend/entity/OrderItem.java
@@ -1,9 +1,16 @@
 package com.example.backend.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
