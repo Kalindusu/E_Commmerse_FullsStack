5c6176eb53a30783c649ba46638b5621ba08c192
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2025-01-08 00:08:50 +0530

    product update

--- backend/src/main/java/com/example/backend/entity/Product.java
+++ backend/src/main/java/com/example/backend/entity/Product.java
@@ -3,11 +3,12 @@ package com.example.backend.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
 @Entity
-@Table(name = 'products')
+@Table(name = "products")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,7 +17,9 @@ public class Product {
     private  String name;
     private  String description;
     private  String imageUrl;
-
+    private BigDecimal price;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
     private  Category category;
     @Column(name = "created_at")
     private final LocalDateTime createdAt=LocalDateTime.now();
